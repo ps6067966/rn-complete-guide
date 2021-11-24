6b947ae56cde24f670c0408547048bcbe394f90e
Pratap Singh <ps6067966@gmail.com>
2021-11-24 12:55:53 +0530

    used style sheet object instead inline style

--- App.js
+++ App.js
@@ -4,12 +4,9 @@ import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ padding: 50 }}>
-      <View>
-        <TextInput
-          placeholder="Course Goal"
-          style={{ borderColor: "black", borderWidth: 1, padding: 10 }}
-        />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} />
         <Button title="ADD" />
       </View>
       <View></View>
@@ -17,4 +14,19 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  },
+});
